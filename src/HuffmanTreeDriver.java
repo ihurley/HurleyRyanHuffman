d07a0a8da540a8ab0534676f49e951417201c4b3
ldaniel18 <ldaniel18@kentdenver.org>
2018-12-04 22:02:13 -0700

    reading code and encoding

---
+++ src/HuffmanTreeDriver.java
@@ -0,0 +1,26 @@
+/**
+ * Name: Izzy Hurley
+ * Lab Name: HuffmanTreeDriver
+ * Lab Purpose: The goal of HuffmanTreeDriver is to create a program
+ * Date: 11/30/18
+ * Collaborators: None
+ * ON MY HONOR: IH
+ */
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.File;
+public class HuffmanTreeDriver {
+    public static void main(String[] args) throws FileNotFoundException{
+    Scanner s = new Scanner(new File("sample.txt"));
+        String string = s.nextLine();
+        System.out.println(string);
+        HuffmanTree hf = new HuffmanTree(string);
+
+        System.out.println(hf.size);
+        System.out.println(hf);
+
+System.out.print(hf.buildEncoding());
+
+
+    }
+}
